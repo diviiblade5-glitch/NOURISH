ed3b089d879b1276a7db285dec4b6427fc93ace2
diviiblade5-glitch <diviblade5@gmail.com>
2026-09-15 05:08:06 +0100

    Nourish

---
+++ db.py
@@ -0,0 +1,61 @@
+foods = [
+    {
+        "name": "Rice",
+        "country": "Nigeria",
+        "classes": ["Carbohydrate"],
+        "serving_grams": 100,
+        "calories": 130,
+        "protein": 2.7,
+        "carbohydrates": 28,
+        "fat": 0.3,
+        "fiber": 0.4,
+        "vitamins": ["B1", "B3"],
+        "minerals": ["Iron", "Magnesium"],
+        "water": 68
+    },
+
+    {
+        "name": "Beans",
+        "country": "Nigeria",
+        "classes": ["Protein", "Carbohydrate"],
+        "serving_grams": 100,
+        "calories": 127,
+        "protein": 8.9,
+        "carbohydrates": 23.7,
+        "fat": 0.5,
+        "fiber": 6.4,
+        "vitamins": ["B1", "B6", "Folate"],
+        "minerals": ["Iron", "Magnesium", "Potassium"],
+        "water": 67
+    },
+
+    {
+        "name": "Egg",
+        "country": "Nigeria",
+        "classes": ["Protein", "Fat"],
+        "serving_grams": 100,
+        "calories": 143,
+        "protein": 12.6,
+        "carbohydrates": 0.7,
+        "fat": 9.5,
+        "fiber": 0,
+        "vitamins": ["A", "D", "B12"],
+        "minerals": ["Iron", "Phosphorus", "Selenium"],
+        "water": 76
+    },
+
+    {
+        "name": "Sweet Potato",
+        "country": "Nigeria",
+        "classes": ["Carbohydrate", "Vitamin"],
+        "serving_grams": 100,
+        "calories": 86,
+        "protein": 1.6,
+        "carbohydrates": 20.1,
+        "fat": 0.1,
+        "fiber": 3.0,
+        "vitamins": ["A", "C", "B6"],
+        "minerals": ["Potassium", "Manganese"],
+        "water": 77
+    }
+]
--- nourish.py
+++ nourish.py
@@ -1,13 +1,101 @@
+from db import foods
+
+
+
 print("================================")
 print("           NOURISH")
 print("    Personalized Food Planner")
 print("================================")
 
-name = input("What is your name? ")
-date_of_birth = input("What is your date of birth? ")
-country = input("What country do you live in? ")
-occupation = input("What type of work do you do? ")
-goal = input("What is your nutrition goal? ")
+name = input("What is your name?: ")
+date_of_birth = input("What is your date of birth?: ")
+country = input("What country do you live in?: ")
+
+print()
+print(f"What best describes your usual work activity?\n"
+      f"1. Mostly sitting or desk work\n"
+      f"2. Mostly standing or light walking\n"
+      f"3. Physically demanding work\n"
+      f"4. Regular sports or gym training\n"
+      f"5. Night-shift work\n"
+      f"6. My activity varies")
+
+activity = input("Choose an option (1-6): ")
+
+
+if activity == "1":
+    activity_type = "Low physical activity"
+
+elif activity == "2":
+    activity_type = "Light physical activity"
+
+elif activity == "3":
+    activity_type = "High physical activity"
+
+elif activity == "4":
+    activity_type = "High physical activity"
+
+elif activity == "5":
+    activity_type = "Night-shift work"
+
+elif activity == "6":
+    activity_type = "Variable activity"
+
+else:
+    activity_type = "Not specified"
+
+
+print()
+print(f"What is your nutrition goal?\n"
+f"1. Eat healthier\n"
+f"2. Lose weight\n"
+f"3. Gain weight\n" 
+f"4. Build muscle\n"
+f"5. Maintain my current weight\n"
+f"6. Create a regular eating schedule")
+
+goal = input("Choose an option (1-6): ")
+
+
+if goal == "1":
+    nutrition_goal = "Eat healthier"
+
+elif goal == "2":
+    nutrition_goal = "Lose weight"
+
+elif goal == "3":
+    nutrition_goal = "Gain weight"
+
+elif goal == "4":
+    nutrition_goal = "Build muscle"
+
+elif goal == "5":
+    nutrition_goal = "Maintain my current weight"
+
+elif goal == "6":
+    nutrition_goal = "Create a regular eating schedule"
+
+else:
+    nutrition_goal = "Not specified"
+
+
+if nutrition_goal == "Build muscle":
+    nutrition_focus = "Protein and carbohydrates"
+
+elif nutrition_goal == "Gain weight":
+    nutrition_focus = "Nutrient-rich, energy-dense foods"
+
+elif nutrition_goal == "Lose weight":
+    nutrition_focus = "Nutrient-dense foods and appropriate portions"
+
+elif nutrition_goal == "Maintain my current weight":
+    nutrition_focus = "Balanced nutrition"
+
+elif nutrition_goal == "Eat healthier":
+    nutrition_focus = "A balanced variety of nutrients"
+
+else:
+    nutrition_focus = "Balanced nutrition"
 
 print()
 print("================================")
@@ -17,9 +105,41 @@ print("================================")
 print("Name:", name)
 print("Date of Birth:", date_of_birth)
 print("Country:", country)
-print("Occupation:", occupation)
-print("Goal:", goal)
+print("Work Activity:", activity_type)
+print("Goal:", nutrition_goal)
+print("Nutrion_goal:", nutrition_focus)
 
 print()
 print("Nourish is preparing your")
 print("personalized recommendations...")
+
+print()
+print("Foods currently available to Nourish:")
+
+for food in foods:
+    print("-", food["name"])
+
+print()
+print("==============================")
+print("     NOURISH RECOMMENDATIONS")
+print("==============================")
+
+print("Based on your nutrition goal:")
+print(nutrition_goal)
+print()
+
+for food in foods:
+    if nutrition_goal == "Build muscle":
+        if "Protein" in food["classes"]:
+            print("-", food["name"])
+
+    elif nutrition_goal == "Gain weight":
+        if food["calories"] >= 120:
+            print("-", food["name"])
+
+    elif nutrition_goal == "Lose weight":
+        if food["calories"] <= 130:
+            print("-", food["name"])
+
+    else:
+        print("-", food["name"])
